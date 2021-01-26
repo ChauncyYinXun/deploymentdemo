b2476bca4da8ee3abae63b06f9a6f9e72045bee2
chauncy <xy2173@columbia.edu>
2021-01-26 21:03:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyle = makeStyles(theme => ({
+const useStyle = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
-  },  
+    flexGrow: 1,
+  },
   appBar: {
-    alignItems: 'center',
-    marginBottom: '2rem'
-  }
+    alignItems: "center",
+    marginBottom: "2rem",
+  },
 }));
 
 function App() {
@@ -19,17 +19,15 @@ function App() {
 
   return (
     <div className={classes.root}>
-     <AppBar position="static" className={classes.appBar} >
-       <Toolbar>
-         <Typography variant="h5">
-          Learning app
-         </Typography>
-       </Toolbar>
-     </AppBar>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h5">Learning app</Typography>
+        </Toolbar>
+      </AppBar>
       <Typography align="center" variant="body1">
-         A simple starter app to hack around with new concepts and technologies for learning purposes.
+        A simple deployment demo app to hack around with new concepts and
+        technologies for learning purposes.
       </Typography>
-
     </div>
   );
 }
